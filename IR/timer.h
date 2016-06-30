5fe2ce5c8033a19a245bbaac98e52c98be34ca2f
fizicko <fizicko@yandex.ru>
2016-06-30 08:31:53 +0300

    Update timer.h

--- IR/timer.h
+++ IR/timer.h
@@ -7,7 +7,7 @@
 //
 //  Compiler....: GNU GCC
 //
-//  Description.: Модуль для приемка ИК сигналов. Протокол фирмы NEC  
+//  Description.: ГЊГ®Г¤ГіГ«Гј Г¤Г«Гї ГЇГ°ГЁГҐГ¬ГЄГ  Г€ГЉ Г±ГЁГЈГ­Г Г«Г®Гў. ГЏГ°Г®ГІГ®ГЄГ®Г« ГґГЁГ°Г¬Г» NEC  
 //
 //  Data........: 18.04.11  
 //
@@ -21,11 +21,6 @@
 
 #define FCPU 16UL
 
-#define baudrate 9600L
-#define bauddivider (F_CPU/(16*baudrate)-1)
-#define HI(x) ((x)>>8)
-#define LO(x) ((x)& 0xFF)
-
 #define START_IMP_TH  (12000UL*(F_CPU/1000000))/PRE
 #define START_IMP_MAX (15000UL*(F_CPU/1000000))/PRE
 #define BIT_IMP_MAX   (3000UL*(F_CPU/1000000))/PRE
@@ -38,9 +33,6 @@
 #define MAX_SIZE 5
 #define CAPTURE    0
 
-void TIM_Init(void);       //инициализация таймера Т1
-void TIM_Display(void);    //Вывод принятого кода на дисплей
-void UART_Init(void);
+void TIM_Init(void);       //eieoeaeecaoey oaeia?a O1
 
 #endif //TIM_H#ifndef TIM_H
-
