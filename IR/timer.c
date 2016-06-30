276c431ba7aeb87575ffe4dcf0fd6f6928745909
fizicko <fizicko@yandex.ru>
2016-06-30 09:24:50 +0300

    Update timer.c

--- IR/timer.c
+++ IR/timer.c
@@ -2,14 +2,14 @@
 
 #include "timer.h"
 
-unsigned char buffer_index=0;
-unsigned int period;
-static unsigned char data; 
+unsigned char buffer_index=0;	//счетсик для массива принятых данных
+unsigned int period;		//переменная для пересчета задержки
+static unsigned char data; 	
 static unsigned char countBit, countByte;
 volatile unsigned int icr1 = 0;
 volatile unsigned int icr2 = 0;
 
-volatile unsigned char flag = 0;
+volatile unsigned char flag = 0;	
 
 enum state {IDLE = 0, RESEIVE = 1};  
 enum state currentState = IDLE;
@@ -17,8 +17,7 @@ enum state currentState = IDLE;
 unsigned char buf[MAX_SIZE];
 
 //***************************************
-
-
+//	Настройнка таймера
 //***************************************
 void TIM_Init(void)
 {
@@ -35,60 +34,53 @@ void TIM_Init(void)
 }
 
 //***************************************
+//	Прерывание по захвату
+//***************************************
 
 ISR(TIMER5_CAPT_vect)
 {
-
-   icr1 = icr2;
-   icr2 = ICR5;  
-
+	icr1 = icr2;
+	icr2 = ICR5;
 	period = icr2;
 	
-  if (icr2 > icr1) {
-    period -= icr1;
-  }
-  else {
-    period += (65535 - icr1);    
-  }	
+	if (icr2 > icr1) {
+		period -= icr1;
+	}
+	else {
+	period += (65535 - icr1);
+	}	
 	
 	switch(currentState){
-      
-      case IDLE:
-	  
-         if (period < START_IMP_MAX) {
-		
-           if (period > START_IMP_TH){
-		   //
-             data = 0;
-             countBit = 0;
-             countByte = 0;
-             buf[NUM_REPEAT] = 0;
-             currentState = RESEIVE;
-           }
-           else {
-             buf[NUM_REPEAT]++;
-           }
-         }
-         break;
+		case IDLE:
+			if (period < START_IMP_MAX) {
+				if (period > START_IMP_TH){
+					data = 0;
+					countBit = 0;
+					countByte = 0;
+					buf[NUM_REPEAT] = 0;
+					currentState = RESEIVE;
+				}
+			else {
+				buf[NUM_REPEAT]++;
+				}
+			}
+			break;
        
-       case RESEIVE:
-	   
-         if (period < BIT_IMP_MAX){
-           if (period > BIT_IMP_TH){		   
-              SetBit(data, 7);
-           }
-           countBit++;
-           if (countBit == 8){
-             buf[countByte] = data;
-             countBit = 0;
-             data = 0;
-             countByte++;
-			 
-             if (countByte == (MAX_SIZE - 1)){
-
-               SetBit(flag, RESEIVE_OK);
-               currentState = IDLE;
-               break;
+		case RESEIVE:
+			if (period < BIT_IMP_MAX){
+				if (period > BIT_IMP_TH){		   
+					SetBit(data, 7);
+				}
+					 countBit++;
+				if (countBit == 8){
+					buf[countByte] = data;
+					countBit = 0;
+					data = 0;
+					countByte++;
+				 if (countByte == (MAX_SIZE - 1)){
+					SetBit(flag, RESEIVE_OK);
+					currentState = IDLE;
+			break;
              }             
            }
            data = data>>1;
@@ -101,10 +93,4 @@ ISR(TIMER5_CAPT_vect)
     }
    
 }
-
-
-
-
 //***************************************
-
-
